7e2bc959fc63d014e9a2e91db42b74756dd3196a
Thomas Kammerer <thomas.kammerer@th-nuernberg.de>
2020-11-27 12:52:41 +0100

    Revert "Feedback (#1)"
    This reverts commit 452625cd5d8438eb4976a81cb4a6315c15f8c31b.

--- LifeTest.java
+++ LifeTest.java
@@ -13,60 +13,26 @@ public class LifeTest {
         l.setAlive(0, 2);
 
         // Act: Berechnung der Folgegeneration
-        l.nextGeneration();
+        ILife nextGen = l.nextGeneration();
 
         // Assert: Rasterpunkt mit drei Nachbarn sollte jetzt leben
-        assertTrue("Error: cell not alive", l.checkAlive(1, 1));
+        assertTrue(nextGen.isAlive(1, 1));
     }
 
 
     @Test
     public void destroyLonelyCell() {
-
-      Life l = new Life();
-      l.setAlive(2,2);
-      l.nextGeneration();
-      assertFalse(l.checkAlive(1, 1));
-     
     }
 
 
     @Test
     public void keepAliveCell() {
-
-      Life l = new Life();
-      l.setAlive(2,3);
-      l.setAlive(1,3);
-      l.setAlive(2,4);
-      l.nextGeneration();
-      assertTrue(l.checkAlive(2, 4));
-
-      Life w = new Life();
-
-      w.setAlive(2,3);
-      w.setAlive(1,3);
-      w.setAlive(3,3);
-      w.setAlive(2,4);
-
-      w.nextGeneration();
-      assertTrue("Error: cell not alive", w.checkAlive(2, 4));
-
-     
     }
 
 
     @Test
     public void destroyCrowdedCell() {
-      Life w = new Life();
-
-      w.setAlive(2,3);
-      w.setAlive(1,3);
-      w.setAlive(3,3);
-      w.setAlive(2,4);
-      w.setAlive(3,4);
-      w.nextGeneration();
-      assertFalse("Error: cell alive", w.checkAlive(2, 4));
     }
-  
+
 
 }
